182d819f06ed0cacd861e6341e890910e78c21b2
Trishita-dcoder <92671123+Trishita-dcoder@users.noreply.github.com>
2022-01-30 01:32:07 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var balloon, background;
+function preload() {
+  backgroundImg = loadImage("Images/1.png")
+  balloonImage = loadAnimation("Images/2.png", "Images/3.png", "Images/4.png");
+}
+
+function setup(){
+   database = firebase.database();
+   console.log(database);
+    createCanvas(500,500);
+
+balloon = createSprite (100,400, 20, 20)
+  balloon.addAnimation("balloon", balloonImage);
+  balloon.scale = 0.4;
+
+
+}
+
+function draw(){
+
+    background(backgroundImg);
+  
+        if(keyDown(LEFT_ARROW)){
+            balloon.x = balloon.x - 10;
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            balloon.x = balloon.x + 10;
+        }
+        else if(keyDown(UP_ARROW)){
+            balloon.y = balloon.y - 10;
+        }
+        else if(keyDown(DOWN_ARROW)){
+             balloon.y = balloon.y + 10;
+        }
+        drawSprites();
+    }
